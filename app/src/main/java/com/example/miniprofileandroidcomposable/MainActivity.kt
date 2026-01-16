865f1ed1701a05c03ad24aeb1ccb698b74b8a134
Jeff Rouzel Bat-og <96866827+jeffrouzel@users.noreply.github.com>
2026-01-16 16:49:22 +0800

    Complete fix of layout

--- app/src/main/java/com/example/miniprofileandroidcomposable/MainActivity.kt
+++ app/src/main/java/com/example/miniprofileandroidcomposable/MainActivity.kt
@@ -5,10 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -16,36 +13,28 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.miniprofileandroidcomposable.ui.theme.MiniProfileAndroidComposableTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,6 +45,8 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+///////////////////////////////////////////////////////////////
+// EDITS
 data class AS(val name: String)
 data class HAS(val name: String)
 
@@ -70,25 +61,50 @@ val helpAndSupport = listOf(
     HAS("Terms and Conditions"),
     HAS("FAQ and Help")
 )
-
-// Modifiers
-// TEXT INFORMATION
+///////////////////////////////////////////////////////////////
+// Styling
+// TEXT Styling
 val textInformationModifier = Modifier
     .padding(
         horizontal = (8.dp),
         vertical = (8.dp)
     )
-
 val textInformationStyle = TextStyle(
     fontSize = (24.sp),
     color = Color.Black
 )
-// TEXT LABEL
 val textLabelStyle = TextStyle(
     fontSize = (16.sp),
     color = Color.Gray
 )
 
+
+// USER Styles
+val userNameStyle = TextStyle(
+    fontSize = (32.sp),
+    color = Color.Black
+)
+val userImageModifier = Modifier
+    .size(150.dp) // Set a specific size
+    .clip(CircleShape)
+
+
+// Spacers
+val spacerNormalVerticalModifier = Modifier
+    .height(20.dp)
+val spacerSmallHorizontalModifier = Modifier
+    .width(10.dp)
+val spaceFillerWidthModifier = Modifier
+    .fillMaxWidth()
+val spaceFillerHeightModifier = Modifier
+    .fillMaxWidth()
+
+
+// Button Styles
+val openInfoButtonModifier = Modifier
+    .size(36.dp)
+
+///////////////////////////////////////////////////////////////
 // Composables
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
@@ -103,26 +119,24 @@ fun Header(){
             //
             item{ UserDetails() }
             // Account Settings
-            item{Spacer(modifier = Modifier.height(20.dp))}
+            item{Spacer(modifier = spacerNormalVerticalModifier)}
             item{Text("Account Settings" , style = textLabelStyle)}
             items(accountSettings){ accountSettings ->
-                Text(text = accountSettings.name,
-                    modifier = textInformationModifier,
-                    style = textInformationStyle
-                    )
+                InfoSettings(accountSettings.name)
             }
             // Help and Support Settings
-            item{Spacer(modifier = Modifier.height(20.dp))}
+            item{Spacer(modifier = spacerNormalVerticalModifier)}
             item{Text("Help and Support", style = textLabelStyle)}
             items(helpAndSupport){ helpAndSupport ->
-                Text(text = helpAndSupport.name,
-                    modifier = textInformationModifier,
-                    style = textInformationStyle)
-
+                InfoSettings(helpAndSupport.name)
             }
             // Log Out Button
-            item{Spacer(modifier = Modifier.height(20.dp))}
-            item{Button(onClick = { /*TODO*/ }) {
+            item{Spacer(modifier = spacerNormalVerticalModifier)}
+            item{Button(onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Red,
+                contentColor = Color.White // or whatever color you want for the icon
+            )) {
                 Text(text = "Logout")
             }}
         }
@@ -131,20 +145,41 @@ fun Header(){
 
 @Composable
 fun UserDetails(){
-    Column (modifier = Modifier
-        .fillMaxWidth(),
-        verticalArrangement = Arrangement.spacedBy(5.dp, Alignment.CenterVertically),
+    Column (modifier = spaceFillerHeightModifier,
+        verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
         horizontalAlignment = Alignment.CenterHorizontally){
         Image(
             painter = painterResource(id = R.drawable.user_icon),
             contentDescription = stringResource(id = R.string.user_icon),
-            modifier = Modifier
-                .size(150.dp) // Set a specific size
-                .clip(CircleShape), // This makes it round
+            modifier = userImageModifier, // This makes it round
             contentScale = ContentScale.Crop // Important: Crops the image to fill the circle
         )
-        Text(text = "Jeff Rouzel Bat-og")
-        Text(text = "jbatog2@77global.biz")
-        Text(text = "Check out my Profile!")
+        Text(text = "Jeff Rouzel Bat-og", style = userNameStyle)
+        Text(text = "jbatog2@77global.biz", style = textLabelStyle)
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "View Profile")
+        }
+    }
+}
+
+@Composable
+fun InfoSettings(textlbl: String){
+    Row{
+        Spacer(modifier = spacerSmallHorizontalModifier)
+        Row (horizontalArrangement = Arrangement.SpaceBetween, modifier = spaceFillerWidthModifier){
+            Text(text = textlbl,
+                modifier = textInformationModifier,
+                style = textInformationStyle)
+            Button(onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent,
+                    contentColor = Color.Black // or whatever color you want for the icon
+                )
+            ) {Icon(painter = painterResource(id = R.drawable.arrow_right),
+                contentDescription = stringResource(id = R.string.arrowRight_desc),
+                modifier = openInfoButtonModifier
+            )
+            }
+        }
     }
 }
